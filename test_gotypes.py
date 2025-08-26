fc5a8bab29b883638ff0359859b1320cc33513e5
Urs Mayr <mayr.urs@gmail.com>
2025-08-26 19:29:06 +0200

    Expand go Player test

--- test_gotypes.py
+++ test_gotypes.py
@@ -2,6 +2,9 @@ from gotypes import Player
 
 def test_player():
     white = Player(1)
+    black = Player(2)
 
     assert white.other == Player.black
+    assert black.other == Player.white
+
     
